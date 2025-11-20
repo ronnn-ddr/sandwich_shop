1958525225006b3f4776661929b34aba73603772
Ronnie Smith-Drake <ronnie.2smithdrake@icloud.com>
2025-11-20 17:58:49 +0000

    Added about page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
+import 'package:sandwich_shop/views/about_screen.dart';
 
 void main() {
   runApp(const App());
@@ -10,9 +11,18 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Sandwich Shop App',
-      home: OrderScreen(maxQuantity: 5),
+      home: const OrderScreen(maxQuantity: 5),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/about':
+            return MaterialPageRoute(builder: (_) => const AboutScreen());
+          default:
+            return MaterialPageRoute(
+                builder: (_) => const OrderScreen(maxQuantity: 5));
+        }
+      },
     );
   }
 }
